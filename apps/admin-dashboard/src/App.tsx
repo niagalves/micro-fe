3e5c6887db859644654d32ae6df68ddf777771de
Niag Alves <niag.souza1993@hotmail.com>
2023-09-24 15:34:59 -0300

    feat: initial commit

---
+++ apps/admin-dashboard/src/App.tsx
@@ -0,0 +1,11 @@
+import Button from 'ui/Button';
+
+function App() {
+  return (
+    <div>
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ packages/ui/src/components/Button/index.tsx
@@ -0,0 +1,9 @@
+export const Button = () => {
+  return (
+    <div>
+      <button style={{ background: 'red', color: 'white' }}>click</button>
+    </div>
+  );
+};
+
+export default Button;
